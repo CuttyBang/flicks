77cecb4b4f48c9d36ec55829ea4ead3de5b25f6a
Nathan McElwain <nathanmcelwain@ymail.com>
2016-09-15 15:56:00 -0700

    trying to fix loading screen again

--- _build/js/structure.js
+++ _build/js/structure.js
@@ -1,10 +1,13 @@
+/*
+
 const $ = require('jquery');
 
 window.setTimeout(()=>{$('.loading-page').fadeOut('fast');}, 1000);
 
-/*
+
 $("#menu-toggle").click((e)=> {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
 */
